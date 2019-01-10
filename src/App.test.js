36a3503dd46bb74db2f2ee579f6b5e158b05cb52
hando <enthusapp@gmail.com>
2019-01-10 13:56:46 +0900

    first test, set document title

--- src/App.test.js
+++ src/App.test.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
+it('set app title', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  window.requestAnimationFrame(() => {
+    expect(document.title).to.equal('Enthus Esign');
+    done();
+  });
+  ReactDOM.unmountComponentAtNode(div);
+});
+
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
