6448d0a63d9df2a25ded75422c1073e59075e187
hando <enthusapp@gmail.com>
2019-01-11 15:33:15 +0900

    remove Button and use button, Button is not necessary

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Player from './components/Player';
 
-function App() {
-  return (
-    <div className="App">
-      <Player
-        text="Text"
-        backgroundColor="black"
-        color="white"
-        height="100px"
-        direction="scroll-up"
-      />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      number: 30,
+    };
+  }
+
+  increase = () => {
+    const { number } = this.state;
+    this.setState(() => ({ number: number + 1 }));
+  }
+
+  render() {
+    const { increase } = this;
+    return (
+      <div className="App">
+        <Player
+          text="Text"
+          backgroundColor="black"
+          color="white"
+          height="100px"
+          direction="scroll-up"
+        />
+        <button
+          type="button"
+          onClick={increase}
+        >
+          {'+'}
+        </button>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { shallow } from 'enzyme';
 import App from './App';
 
 describe('basic app test', () => {
@@ -17,5 +18,27 @@ describe('basic app test', () => {
       expect(document.title).to.equal('Enthus Esign');
     });
     ReactDOM.unmountComponentAtNode(div);
+  });
+
+  describe('component', () => {
+    let component = null;
+
+    it('rendering', () => {
+      component = shallow(<App />);
+    });
+
+    it('button exists', () => {
+      expect(component.find('button').exists()).toBe(true);
+    });
+
+    it('button event', () => {
+      component.find('button').simulate('click');
+      expect(component.state().number).toBe(31);
+    });
+
+    it('button event', () => {
+      component.find('button').simulate('click');
+      expect(component.state().number).toBe(32);
+    });
   });
 });
